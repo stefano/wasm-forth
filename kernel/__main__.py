8615dae233863e9407e6394674ec3fbb1080060a
Stefano Dissegna <stefano.dissegna@gmail.com>
2017-12-26 14:17:56 +0100

    fixed build

--- kernel/__main__.py
+++ kernel/__main__.py
@@ -6,24 +6,33 @@ import assembler
 
 
 BASE_PATH = os.path.abspath(os.path.dirname(__file__))
+DIST_PATH = os.path.join(BASE_PATH, '../dist')
 
-assembler.build_kernel(os.path.join(BASE_PATH, '../dist/kernel.wasm'))
+if not os.path.exists(DIST_PATH):
+    os.makedirs(DIST_PATH)
+
+assembler.build_kernel(os.path.join(DIST_PATH, 'kernel.wasm'))
 shutil.copy(
     os.path.join(BASE_PATH, 'forth/core.f'),
-    os.path.join(BASE_PATH, '../dist/core.f'),
+    os.path.join(DIST_PATH, 'core.f'),
 )
 
 if len(sys.argv) > 1 and sys.argv[1] == '--demo-repl':
     import http.server
     import socketserver
 
+    REPL_DIST_PATH = os.path.join(BASE_PATH, '../repl/dist/')
+
+    if not os.path.exists(REPL_DIST_PATH):
+        os.makedirs(REPL_DIST_PATH)
+
     shutil.copy(
         os.path.join(BASE_PATH, 'forth/core.f'),
-        os.path.join(BASE_PATH, '../repl/dist/core.f'),
+        os.path.join(REPL_DIST_PATH, 'core.f'),
     )
     shutil.copy(
-        os.path.join(BASE_PATH, '../dist/kernel.wasm'),
-        os.path.join(BASE_PATH, '../repl/dist/kernel.wasm'),
+        os.path.join(DIST_PATH, 'kernel.wasm'),
+        os.path.join(REPL_DIST_PATH, 'kernel.wasm'),
     )
 
     os.chdir(os.path.join(BASE_PATH, '..', 'repl'))
